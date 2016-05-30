e900dbfd03597c6bd5bd42a24b651ccd1a83f92d
nlogo <nlogo0816@gmail.com>
2016-05-30 21:52:10 +0800

    Project init

---
+++ SimpleUI/app/src/main/java/domain/company/simpleui/simpleui/MainActivity.java
@@ -0,0 +1,20 @@
+package domain.company.simpleui.simpleui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("!@#$%^&*()");
+        textView.
+    }
+}
