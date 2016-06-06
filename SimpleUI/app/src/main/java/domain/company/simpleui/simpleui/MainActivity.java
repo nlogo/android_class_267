adde0d5145a0705f5a6574a3ce06774e5943d81f
nlogo0816@gmail.com <summer0816>
2016-06-06 19:48:42 +0800

    Today

--- SimpleUI/app/src/main/java/domain/company/simpleui/simpleui/MainActivity.java
+++ SimpleUI/app/src/main/java/domain/company/simpleui/simpleui/MainActivity.java
@@ -4,11 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,9 +21,14 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     RadioGroup radioGroup;
     CheckBox checkBox;
-    String selectedSex = "Male";
-    String name = "";
-    String sex = "";
+    //String selectedSex = "Male";
+    //String name = "";
+    //String sex = "";
+
+    String drinkName = "Black Tea";
+    ListView listView ;
+    ArrayList<String> drinks = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.editText);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
+        listView = (ListView) findViewById(R.id.listView);
+
+        setupListView();
 
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
@@ -44,45 +57,59 @@ public class MainActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                if(i == R.id.Rmale)
-                {
-                    selectedSex = "Male";
+                //if(i == R.id.Rmale)
+                //{
+                    //selectedSex = "Male";
 
-                }else if(i == R.id.Rfemale){
-                    selectedSex = "Female";
-                }
+                //}else if(i == R.id.Rfemale){
+                    //selectedSex = "Female";
+                //}
 
-                sex = selectedSex;
+                //sex = selectedSex;
+                RadioButton radioButton = (RadioButton) findViewById(i);
+                drinkName = radioButton.getText().toString();
             }
         });
 
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
-                changeTextView();
-            }
-        });
+//        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+//
+//                changeTextView();
+//            }
+//        });
       }
+
+    public void setupListView()
+    {
+        String[] data = new String[]{"123", "456", "789", "Hello", "ListView", "Hi"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinks);
+        listView.setAdapter(adapter);
+    }
 
     public void click(View view)
     {
-        name = editText.getText().toString();
-        sex = selectedSex ;
-        changeTextView();
+       String note = editText.getText().toString();
+
+        //sex = selectedSex ;
+        //changeTextView();
+        drinks.add(drinkName);
+
+        textView.setText(drinkName);
         editText.setText("");
+        setupListView();
     }
 
-    public void changeTextView()
-    {
-        if ( name.equals(""))
-            return ;
-        if(checkBox.isChecked()){
-            textView.setText(name);
-        }
-        else{
-            String content = name + " sex:" + sex;
-            textView.setText(content);
-        }
-    }
+//    public void changeTextView()
+//    {
+//        if ( name.equals(""))
+//            return ;
+//        if(checkBox.isChecked()){
+//            textView.setText(name);
+//        }
+//        else{
+//            String content = name + " sex:" + sex;
+//            textView.setText(content);
+//        }
+//    }
 }
